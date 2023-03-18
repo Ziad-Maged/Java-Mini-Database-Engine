870a57236e5bb9f9de6a79034e94d1b1fd59d119
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-03-18 16:16:30 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/Table.java
+++ src/main/Table.java
@@ -62,7 +62,7 @@ public class Table implements Serializable{
         }
     }
 
-    public void insert(String strClusteringKeyValue, Hashtable<String,Object> htblColNameValue){
+    public void insert(Hashtable<String,Object> htblColNameValue){
 
     }
 
