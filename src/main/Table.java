23549f72c24d610ac14567ff07851bb11fe946be
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-03-21 03:10:13 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/Table.java
+++ src/main/Table.java
@@ -70,6 +70,7 @@ public class Table implements Serializable{
         }
     }
 
+    // compareWith(o, e) = 1 if o > e, = -1 if o < e, = 0 if o = e
     private int compareWith(Object o, Object e){
         if(o instanceof Integer && e instanceof Integer)
             return ((Integer)o).compareTo((Integer)e);
@@ -83,20 +84,47 @@ public class Table implements Serializable{
     }
 
     public void insert(String strClustringKey, Hashtable<String,Object> htblColNameValue){
-        boolean shift = false;
-        Page p = null;
+        boolean shift = false; // boolean variable to indicate whether we need to shift or not
+        Page p = null; // preparing a Page varialbe
+        /**
+         * preparing a temp variable to change the min and max values of a page
+         * if needed during the shifting process
+         */
         Hashtable<String, Object> temp = null;
-        for(PageDetails e : details){
-            if(!shift){
+        for(PageDetails e : details){ //looping over the pageDetails vector of the table
+            if(!shift){ // checking if no shift is needed
                 int compareMin = compareWith(htblColNameValue.get(strClustringKey),
-                        e.getMinimumRecord().get(strClustringKey));
+                        e.getMinimumRecord().get(strClustringKey)); // comparing the input record with the minimum record of the page
                 int compareMax = compareWith(htblColNameValue.get(strClustringKey),
-                        e.getMaximumRecord().get(strClustringKey));
-
+                        e.getMaximumRecord().get(strClustringKey)); // comparing the input record with the maximum record of the page
+                if(compareMin == -1 && compareMax == -1){ // if input record is < the minimum and maximum records of the page
+                    p = loadPage(".\\" + DBApp.getStrCurrentDatabaseName() +
+                            "\\" + e.getPageName() + ".class"); //load the current page
+                    p.setName(e.getPageName()); //set the current page name for later serialization
+                    if(e.isFull()){ // checking if the page is already full
+                        shift = true; // setting the shift to true to start the shifting process
+                        temp = p.getRecords().get(p.getRecords().size() - 1); // setting the temp variable to be the maximum record of the page to shift it to the next page
+                        p.getRecords().remove(temp); // removing the last record from the page to maintain the maximum number of records per page
+                        e.setMaximumRecord(p.getRecords().get(p.getRecords().size() - 1)); // resetting the maximum record of the page to be the current last record of that page
+                    }
+                    p.getRecords().insertElementAt(htblColNameValue, 0); // inserting the new record at the beginning of the vector since it is smaller than the minimum record
+                    e.setMinimumRecord(htblColNameValue); // resetting the minimum record to be the input record since it is now the smallest
+                    if(p.isFull()) // checking if the page was not full before insertion and became full after insertion
+                        e.setFull(true); // if true then we indicate that the page is now full and any further insertions in that page require shifting
+                    p.savePage(".\\" + DBApp.getStrCurrentDatabaseName() +
+                            "\\" + e.getPageName() + ".class"); // saving the page after the insertion process
+                }else if(compareMin == 1 && compareMax == -1){
+                    //TODO Binary Search to find the needed index of insertion
+                }else if(compareMin == 1 && compareMax == 1){
+                    //TODO check if the page is not full
+                }
             }else {
-
+                //TODO Shifting process here
             }
         }
+        numberOfRecords++;
+        this.saveTable(".\\" + DBApp.getStrCurrentDatabaseName() +
+                "\\" + tableName + ".class");
     }
 
     public void delete(String strClusteringKeyValue, Hashtable<String,Object> htblColNameValue){
