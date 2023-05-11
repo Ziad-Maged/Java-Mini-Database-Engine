29012f0e17e339e5369ae0b945272803ede7ed5a
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-11 21:28:02 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/index/Point3D.java
+++ src/main/java/index/Point3D.java
@@ -3,7 +3,7 @@ package main.java.index;
 import java.io.Serializable;
 
 public class Point3D implements Serializable {
-    private double x, y, z;
+    private final double x, y, z;
 
     public Point3D(double x, double y, double z){
         this.x = x;
